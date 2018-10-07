e45d1f7a0d90800e0bcf210a899db7c45704a377
xiaoliu670 <xiaoliu670@aliyun.com>
2018-10-07 22:43:24 +0800

    创建新的master分支

---
+++ src/main/java/com/xiaoliu670/springboot/test1/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.xiaoliu670.springboot.test1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/xiaoliu670/springboot/test1/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.xiaoliu670.springboot.test1.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping (value="/login", method= RequestMethod.GET)
+    public String hello(){
+        return "hello world springboot project";
+    }
+}
